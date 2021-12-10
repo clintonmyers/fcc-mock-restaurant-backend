68bedd32553911de5d4a59aec06887676fbbb8fc
Micheal Arsenault <mike2ykme@gmail.com>
2021-12-09 22:28:35 -0600

    updating models and cleaning up configs

--- web/api/routes.go
+++ web/api/routes.go
@@ -8,29 +8,29 @@ import (
 
 var testimonials = []models.Testimonial{
 	{
-		ID:      0,
+		//ID:      0,
 		Title:   "Testimonial 1",
 		Comment: "Comment #1. There is so much to say about this once!!",
 		Rating:  5,
 	},
 	{
-		ID:      1,
+		//ID:      1,
 		Title:   "Testimonial 2",
 		Comment: "Comment #2. There is so much to say about this once!!",
 		Rating:  5,
 	},
 	{
-		ID:      2,
+		//ID:      2,
 		Title:   "Testimonial 3",
 		Comment: "Comment #3",
 		Rating:  3,
 	}, {
-		ID:      3,
+		//ID:      3,
 		Title:   "Testimonial 4",
 		Comment: "Comment #4",
 		Rating:  4,
 	}, {
-		ID:      4,
+		//ID:      4,
 		Title:   "Testimonial 5",
 		Comment: "Comment #5",
 		Rating:  1,
